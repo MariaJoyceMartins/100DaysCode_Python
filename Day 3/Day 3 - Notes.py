f65373ea3beab426722092758ec14c63c3eb3f1e
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-05-11 22:07:33 -0300

    readjusting day 3 folder location

---
+++ Day 3/Day 3 - Notes.py
@@ -0,0 +1,6 @@
+# Challenge - To a Pizza Delivery Program
+
+print("Welcone to Pytohn Pizza Deliveries!")
+size = input("What size pizza do you want? (S, M or L): ")
+pepperoni = input("Do you want pepperoni on your pizza? (Y or N): ")
+
