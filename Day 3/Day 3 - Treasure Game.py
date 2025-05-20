d2d691e4f01732689cbfced53dbc287678397142
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-05-19 22:57:59 -0300

    start of 3 day project

---
+++ Day 3/Day 3 - Treasure Game.py
@@ -0,0 +1,12 @@
+print("Welcome to the game of luck (or bad luck) created by Joy (or sadness, in the case of this game)")
+print("Here, your choice can save your life (or not)")
+side = input("Which side do you want to go, left or right?")
+
+if side == "right":
+    print("\nYou chose the side of acoustics, and now you will relive in your mind the most distressing moment of your life. Game Over!")
+elif side == "left":
+    print("\nYou have chosen the path of tranquility, and now you have the power to choose when to remain calm and collected in any situation.")
+else:
+    print("\nEnter a valid answer")
+
+
