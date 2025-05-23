4183e9cb753859262d11afade25aaf68633d9e1f
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-05-23 10:10:44 -0300

    Day 5 Notes

---
+++ Day 5/Day 5 - Notes.py
@@ -0,0 +1,7 @@
+# Loops
+
+# For Loops
+fruits = ["Apple", "Banana", "Cherry"]
+for fruit in fruits:
+    print(fruit)
+# In this case, the loop went through one item in the list and placed it in the output, and the code went through one of the fruits and placed it in the output. As it is a loop, it goes back and does the same with the next fruit, and so on.
