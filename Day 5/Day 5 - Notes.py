574866a176a760b8ee0d8a16f7018fa6bd0f6ec3
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-05-24 21:35:02 -0300

    Day 5 notes (Loop)

--- Day 5/Day 5 - Notes.py
+++ Day 5/Day 5 - Notes.py
@@ -3,5 +3,10 @@
 # For Loops
 fruits = ["Apple", "Banana", "Cherry"]
 for fruit in fruits:
-    print(fruit)
+    print(fruits)
+    print(fruit + "pie")
 # In this case, the loop went through one item in the list and placed it in the output, and the code went through one of the fruits and placed it in the output. As it is a loop, it goes back and does the same with the next fruit, and so on.
+
+# Calculate the most highest score
+student_scores = [150, 143, 185, 135, 150, 133, 109, 167, 159, 199, 171, 183, 154, 78, 69]
+total_exam_score = sum(student_scores)
