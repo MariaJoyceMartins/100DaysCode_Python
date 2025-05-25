c1863545e1e0cacb8bf2ccc4407c814d8af9cd15
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-05-25 20:42:16 -0300

    notes day 5 - add the sum of score

--- Day 5/Day 5 - Notes.py
+++ Day 5/Day 5 - Notes.py
@@ -7,6 +7,13 @@ for fruit in fruits:
     print(fruit + "pie")
 # In this case, the loop went through one item in the list and placed it in the output, and the code went through one of the fruits and placed it in the output. As it is a loop, it goes back and does the same with the next fruit, and so on.
 
-# Calculate the most highest score
+# Calculate the sum highest score
 student_scores = [150, 143, 185, 135, 150, 133, 109, 167, 159, 199, 171, 183, 154, 78, 69]
 total_exam_score = sum(student_scores)
+
+sum = 0
+for score in student_scores:
+    sum += score
+
+print(sum)
+
