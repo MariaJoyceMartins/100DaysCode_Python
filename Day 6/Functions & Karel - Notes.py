a2d4bc1c59997201ba8ab31bcc48aa99cc0e02aa
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-05-27 21:47:07 -0300

    (Day 6) Start the Functions notes

---
+++ Day 6/Functions & Karel - Notes.py
@@ -0,0 +1,18 @@
+# Functions
+# Defining and Calling Python Functions
+
+#In Python, have diferents functions defined, like:
+print("Hello")
+
+# All the functions pretty work the same, like por example:
+num_char = len("Hello")
+print(num_char)
+
+# if we want to create or define a function, use:
+def my_function():
+    print("Hello")
+    print("Bye")
+
+#After this, calling your function for this be execute when you run
+
+my_function()
