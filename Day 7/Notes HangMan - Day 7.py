84e21b4a213c9bdd597b7f08a4209417afbd38e0
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-06-03 01:09:36 -0300

    Draft 2 code in the game's to-do list

--- Day 7/Notes HangMan - Day 7.py
+++ Day 7/Notes HangMan - Day 7.py
@@ -1,5 +1,13 @@
 #Doing a game HangMan
 
+
+#1 TO-DO: Create a random list of word
+#2 TO-DO: Create a if/else logic of user when he wrong something
+#3 TO-DO: Makes this run in a loop
+#4 TO-DO: Define the total of things that the user can do
+#5 TO-DO: Test if the code are work
+
+#1 TO-DO:
 import random
 import requests
 
@@ -9,3 +17,10 @@ def fetch_random_word():
 
 random_word = fetch_random_word()
 print(random_word)
+
+#2 TO-DO:
+input_letter = ("Try to define the word, for this, reply a letter")
+if input_letter in random_word:
+    input("You got one letter right!, Now that the next)
+else:
+    input("You got one letter wrong!, Now that the next")
