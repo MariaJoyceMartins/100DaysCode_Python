b818deb8e3bb429d4c8a1f0db14c15b3eba645a3
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-06-06 00:45:27 -0300

    V.0 of day 5 (Loops) completed

--- Day 5 - Loops/Day 5 - Project.py
+++ Day 5 - Loops/Day 5 - Project.py
@@ -1,18 +1,29 @@
 # Check the minimum stock
+from numpy.ma.core import minimum
 
-total_stock = input("Please, enter the total stock")
-exit() #This part is the beginning of the program, after that, the user decide if he what to start the program
+total_stock = int(input("Please, enter the total stock: "))
+minimum_stock = int(input("Please, enter the minimum stock: "))
+
 
 def start_of_system():
-    user_decide = input("Do you wanna to start the program?")
+    user_decide = input("Do you wanna to start the program? ")
     if user_decide == "yes":
-        print(stock)
+        print("Welcome to your stock management system!")
     else:
         exit()
 
-
-stock = input("Please, enter the quantity of items removed")
-
 start_of_system()
+
+def operations_system():
+    stock_removed = int(input("Please, enter the stock to be removed: "))
+    actually_stock = total_stock - stock_removed
+    if actually_stock <= minimum_stock:
+        print("You minimum stock is gone, buy more to make sure you don´t run out of anything")
+    else:
+        print(f"You have enough stock, being it {actually_stock}")
+
+
+operations_system()
+
 
 
