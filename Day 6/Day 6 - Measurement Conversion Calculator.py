b69e931cfd3b2a27e67ec4e4dd465ea7496ba415
Maria Joyce Martins Rabelo <mariarabelo800@gmail.com>
2025-06-07 23:54:58 -0300

    Start Project Day 6, doing the calculate of celsius to kelvin

---
+++ Day 6/Day 6 - Measurement Conversion Calculator.py
@@ -0,0 +1,9 @@
+# Measurement Conversion Calculator
+
+def celsius_to_kelvin():
+    celsius = int(input("Type the temperature in Celsius: "))
+    kelvin = celsius + 273
+    print(f"The temperature in Kelvin is {kelvin}")
+
+
+celsius_to_kelvin()
